1b8d631921cd8dbbc9e61e2ea18a173fbd67916b
EL MOUKI OTMANE <otmane.el-mouki@etu.univ-lyon1.fr>
2025-09-19 15:14:30 +0200

    Ajout javadoc et correction checkstyle (oublié dernier commit)

--- cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/controller/Controller.java
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/controller/Controller.java
@@ -1,61 +1,87 @@
 package fr.univ_lyon1.info.m1.cv_search.controller;
-import fr.univ_lyon1.info.m1.cv_search.model.*;
+import fr.univ_lyon1.info.m1.cv_search.model.ApplicantList;
+import fr.univ_lyon1.info.m1.cv_search.model.SelectionStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle.Control;
 
+
+/**
+ * Controller Class used to connect view-model.
+ */
 
 
 public class Controller {
 
-   private final ApplicantList applicantList;
-   private SelectionStrategy strategy;
-   private final List<String> requiredskills = new ArrayList<>();
+    private final ApplicantList applicantList;
+    private SelectionStrategy strategy;
+    private final List<String> requiredskills = new ArrayList<>();
 
-
-   public Controller(ApplicantList applicantList) {
+    /**
+     * Constructor.
+     * @param applicantList
+     */
+    public Controller(final ApplicantList applicantList) {
         this.applicantList = applicantList;
-   }
+    }
 
 
 
    //----Skills----
-   public void addSkill(String skill) {
+
+   /**
+    * Add skill to the list of required skills and notify all observers.
+    * @param skill skill 
+    */
+    public void addSkill(final String skill) {
         requiredskills.add(skill);
         applicantList.notifyObservers();
-   }
+    }
 
-   public void removeSkill(String skill) {
+
+    /**
+     * remove skill from the list of required skills and notify observers.
+     * @param skill skill
+     */
+    public void removeSkill(final String skill) {
         requiredskills.remove(skill);
         applicantList.notifyObservers();
-   }
+    }
 
    //----Strategy----
 
-   public void setStrategy(SelectionStrategy strategy) {
+
+    /**
+     * Choose the strategy and notify observers.
+     * @param strategy strategy picked by the user
+     */
+    public void setStrategy(final SelectionStrategy strategy) {
         this.strategy = strategy;
         applicantList.notifyObservers();
-   }
+    }
 
    //----Search----
 
-   public void search () {
+
+    /**
+     * notify observers when search button pressed.
+     */
+    public void search() {
         applicantList.notifyObservers();
-   }
+    }
 
    //----Getters----
 
-   public List<String> getRequiredSkills() {
+    public List<String> getRequiredSkills() {
         return requiredskills;
-   }
+    }
 
-   public SelectionStrategy getStrategy() {
+    public SelectionStrategy getStrategy() {
         return strategy;
-   }
+    }
 
 
-   }
+}
 
 
 
