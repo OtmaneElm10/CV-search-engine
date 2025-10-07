97a16ec7e52dc262fb1b5816beba75297968032e
TRAD Nedim <nedim.trad@etu.univ-lyon1.fr>
2025-10-07 19:35:10 +0000

    median strategy deleted and expert strategy created

---
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/model/ExpertInAnyStrategy.java
@@ -0,0 +1,41 @@
+package fr.univ_lyon1.info.m1.cv_search.model;
+import java.util.List;
+
+public class ExpertInAnyStrategy implements Strategy {
+
+    private int threshold;
+
+    public ExpertInAnyStrategy(final int threshold) {
+        this.threshold = threshold;
+    }
+
+    /**
+     *
+     * @param applicant the candidate being evaluated
+     * @param selectedSkills the list of required skill
+     * @return this candidate has an expert level of one or more selectedskills
+     */
+    public boolean isSelected(final Applicant applicant, final List<String> selectedSkills) {
+        for (String skill : selectedSkills) {
+            if (applicant.getSkill(skill) >= threshold) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Return label strategy choice.
+     */
+    public String getLabel() {
+        return "EXPERT >= " + threshold;
+
+    }
+
+
+    public int getThreshold() {
+        return threshold;
+
+    }
+
+}
