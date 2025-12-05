7e9f05b2bc0c5d02bcef69954e1d6fcad6aea951
TRAD Nedim <nedim.trad@etu.univ-lyon1.fr>
2025-12-05 21:50:37 +0100

    short factory created

---
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/model/SortStrategyFactory.java
@@ -0,0 +1,28 @@
+package fr.univ_lyon1.info.m1.cv_search.model;
+
+/**
+ * Factory to create the short strategy.
+ */
+
+public class SortStrategyFactory {
+
+    /**
+     * Create a strategy based on the type
+     * @param type is the type of short choosed
+     * @return the strategy or null if nothing has been choose
+     */
+    public static SortStrategy createSortStrategy(SortType type) {
+        switch (type) {
+            case EXPERIENCE_DESC:
+                return new SortByExperienceDesc();
+            // case NAME_ASC: return new SortByName();
+
+            case NONE:
+            default:
+                return null;
+        }
+    }
+
+
+
+}
